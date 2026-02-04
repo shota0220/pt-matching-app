553d58b558668a603255c4da7a9743509466dc6a
翔大 <roadbike0220@gmail.com>
2026-02-04 14:44:39 +0900

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,13 @@
+export default function RootLayout({children,}: {children: React.ReactNode;}) {
+    
+  return (
+    <html lang="ja">
+        <body>
+            <header style={{ padding: "20px", borderBottom: "1px solid #eee", backgroundColor: "#fff" }}>
+                <strong>PT Matching App</strong>
+            </header>
+            {children}
+        </body>
+    </html>
+  );
+}
